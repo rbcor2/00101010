dd62705231d7383044b1b1981743f3d806c94e72
Rabiul Islam <rb60041@gmail.com>
2017-08-21 07:56:36 +0600

    added threadpoolexecutor

--- sarahah/views.py
+++ sarahah/views.py
@@ -1,22 +1,31 @@
+from concurrent.futures import ThreadPoolExecutor
+
 from django.shortcuts import render
-from django.core.urlresolvers import reverse
 from django.core.mail import send_mail
 
 from .forms import MessageForm
+
+
+executor = ThreadPoolExecutor(10)
+
 
 def sarahah_view(request):
     if request.method == "POST":
         form = MessageForm(request.POST)
         sent = False
         if form.is_valid():
-            print('form valid')
-            subject = "SAHARAH MESSAGE FROM WEBSITE"
-            body  = form.cleaned_data.get('body','no message body found')
-            sender = 'o1935926686@gmail.com'
-            recevers = ['rb60041@gmail.com']
-            sent = send_mail(subject, body, sender, recevers, fail_silently=False)
+
+            kwargs = {}
+            kwargs['subject'] = "SAHARAH MESSAGE FROM WEBSITE"
+            kwargs['message']  = form.cleaned_data.get('body','no message body found')
+            kwargs['from_email'] = 'o1935926686@gmail.com'
+            kwargs['recipient_list'] = ['rb60041@gmail.com']
+            kwargs['fail_silently'] = False
+
+            sent = executor.submit(send_mail, **kwargs)
+
             if sent:
-                sent = True
+                sent=True
         return render(request, 'sarahah/home.html', {'sent':sent, 'form':form})
 
     else:
